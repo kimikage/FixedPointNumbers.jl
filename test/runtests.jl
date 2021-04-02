67d37e13fd95a752f4ff0600633ed8782bc79a34
kimikage <kimikage.ceo@gmail.com>
2021-04-02 12:19:45 +0900

    [DoNotMerge] debug Float16(::N2f14)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,32 @@
 using FixedPointNumbers, Test
 
+using InteractiveUtils
+
+@code_native Float16(zero(N2f14))
+
+@code_native Float16(0x0001 / BigFloat(16383))
+
+@code_native Float16(1.0)
+
+@code_native Float16(1.0f0)
+
+@show Float16(big"16378"/16383)
+@show Float16(big"16379"/16383)
+
+@show Float64(big"16378"/16383)
+@show Float64(big"16379"/16383)
+
+@show Float16(0.9996948055911615)
+@show Float16(0.9997558444729292)
+
+N = N2f14
+T = UInt16
+for i = typemin(T):typemax(T)
+    f_expected = Float16(i / BigFloat(FixedPointNumbers.rawone(N)))
+    f_actual = Float16(reinterpret(N, i))
+    f_actual === f_expected || error("$i, $f_actual, $f_expected")
+end
+#=
 if VERSION >= v"1.6.0-DEV.816" # JuliaLang/julia #36962 # FIXME
     @test isempty(detect_ambiguities(FixedPointNumbers))
 else
@@ -21,3 +48,4 @@ end
 @testset "traits" begin
     include("traits.jl")
 end
+=#
