e20b6b2e9c4f3d745d545e71d3815c82ca9c4745
kimikage <kimikage.ceo@gmail.com>
2026-09-13 19:25:13 +0900

    [WIP][DoNotMerge] Derive the rounding functions from the 2-/3-arg `round`

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,34 @@
-using FixedPointNumbers, Test, Aqua
-
+using FixedPointNumbers, Test#, Aqua
+#=
 Aqua.test_all(FixedPointNumbers)
 
 if Sys.ARCH === :x86_64 || Sys.ARCH === :i686
     using Documenter
     doctest(FixedPointNumbers, manual = false)
+end
+=#
+struct MyFixed <: FixedPoint{Int, 0} end
+ns = rand(N0f8, 10^6)
+qs = min.(rand(Q1f6, 10^6), 1Q1f6)
+
+for f in (floor, ceil, trunc, round)
+    println("Testing function: ", f)
+    GC.gc()
+    f.(ns); f.(Int, ns)
+    @time f.(ns)
+    @time f.(Int, ns)
+    f.(qs); f.(Int, qs)
+    @time f.(qs)
+    @time f.(Int, qs)
+    try
+        f(Int, MyFixed())
+    catch
+        if Base.isdefined(Base,:current_exceptions)
+            Base.display_error(stderr, current_exceptions())
+        else
+            display(stacktrace(catch_backtrace()))
+        end
+    end
 end
 
 @testset "normed" begin
